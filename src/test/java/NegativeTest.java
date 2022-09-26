eef7f298d8706fa5a554ea0cfad5cfbfdf9f724a
Владислав Болдырев <vboldyrev@uma.tech>
2022-09-26 21:01:51 +0300

    add positive, negative tests

---
+++ src/test/java/NegativeTest.java
@@ -0,0 +1,27 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class NegativeTest {
+
+    @Test
+    void test00() {
+        assertFalse(false);
+    }
+
+    @Test
+    void test01() {
+        assertFalse(false);
+    }
+
+    @Test
+    void test02() {
+        assertFalse(false);
+    }
+
+    @Test
+    void test03() {
+        assertFalse(false);
+    }
+}
---
+++ src/test/java/PositiveTest.java
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class PositiveTest {
+
+    @Test
+    void test00() {
+        assertTrue(true);
+    }
+
+    @Test
+    void test01() {
+        assertTrue(true);
+    }
+
+    @Test
+    void test02() {
+        assertTrue(true);
+    }
+
+    @Test
+    void test03() {
+        assertTrue(true);
+    }
+}
