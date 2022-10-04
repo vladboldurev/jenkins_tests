be8ba4ebc16c6ee04c54bfefb7a4634950dae1e0
Владислав Болдырев <vboldyrev@uma.tech>
2022-10-04 18:28:53 +0300

    addsystem properties test

---
+++ src/test/java/properties/SystemPropertiesTest.java
@@ -0,0 +1,15 @@
+package properties;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+
+@Tag("systemProprties")
+public class SystemPropertiesTest {
+
+    @Test
+    void systemTest() {
+        String browser = System.getProperty("browser");
+        System.out.println("browser: " + browser);
+    }
+}
